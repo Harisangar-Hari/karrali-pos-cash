9567439eaefdc065d92e5a33271dea01b2940f82
Harisangar <sangarhari10@gmail.com>
2026-08-07 08:43:16 +0530

    initial commit4

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: "https://pos-nest-k5pm.onrender.com/api",
-    baseURL: "http://localhost:3003/api",
+    baseURL: "https://karrali-pos-backend.onrender.com/api",
+    //baseURL: "http://localhost:3003/api",
 });
